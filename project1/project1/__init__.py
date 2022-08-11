087e7baeef60dfa5605ca326869691d1102d654e
Hironori Yamamoto <mr.nikoru918@gmail.com>
2022-08-12 00:04:53 +0900

    init

---
+++ project1/project1/__init__.py
@@ -0,0 +1,7 @@
+import lightgbm as lgb
+import boto3
+import pandas as pd
+import numpy as np
+
+def hello_world() -> str:
+    "hello world"
---
+++ project2/project2/main.py
@@ -0,0 +1,6 @@
+from project1 import hello_world
+
+print(hello_world())
+
+# comment out/in to check third-party cache works
+# print("bar")
